6a2b3223b0660a507fca78d3af72a60b2235821f
Rodion <piperov2004@mail.ru>
2024-01-10 17:20:57 +0300

    remove extra files and variable

--- src/main/kotlin/ConsoleTrainer.kt
+++ src/main/kotlin/ConsoleTrainer.kt
@@ -9,9 +9,7 @@ fun main() {
     val lines = wordsFile.readLines()
 
     val dictionary: MutableList<Word> = mutableListOf()
-    var countOfLines = 0
     for (line in lines) {
-        countOfLines++
         val line = line.split("|")
         val word = Word(line[0], line[1], line[2].toInt())
         dictionary.add(word)
@@ -24,8 +22,8 @@ fun main() {
         when (choice) {
             1 -> TODO("Добавить функционал")
             2 -> {
-                val percentsOfCorrectAnswers = ((quantityOfCorrectAnswers.size.toDouble() / countOfLines) * 100).toInt()
-                println("Выучено ${quantityOfCorrectAnswers.size} из $countOfLines слов | ${percentsOfCorrectAnswers}%")
+                val percentsOfCorrectAnswers = ((quantityOfCorrectAnswers.size.toDouble() / dictionary.size) * 100).toInt()
+                println("Выучено ${quantityOfCorrectAnswers.size} из ${dictionary.size} слов | ${percentsOfCorrectAnswers}%")
             }
 
             0 -> return

b5d64efa43f5a9f9ffc97bb53ee6620ab99249ae
Rodion <piperov2004@mail.ru>
2024-01-14 18:22:59 +0300

    Added unfinished word learning feature

--- src/main/kotlin/ConsoleTrainer.kt
+++ src/main/kotlin/ConsoleTrainer.kt
@@ -14,15 +14,31 @@ fun main() {
         val word = Word(line[0], line[1], line[2].toInt())
         dictionary.add(word)
     }
+    println(dictionary)
 
     val quantityOfCorrectAnswers = dictionary.filter { it.countOfCorrectAnswer >= 3 }
     while (true) {
         println("Меню: 1 – Учить слова, 2 – Статистика, 0 – Выход")
         val choice = readln().toIntOrNull() ?: continue
         when (choice) {
-            1 -> TODO("Добавить функционал")
+            1 -> {
+                do {
+                    val listOfUnlearnedWords = dictionary.filter { it.countOfCorrectAnswer <= 3 }
+                    if (listOfUnlearnedWords.isEmpty()) println("Вы выучили все слова")
+                    else {
+                        val wordForLearning = listOfUnlearnedWords.random()
+                        val answers = listOfUnlearnedWords.shuffled().take(4)
+                        println(wordForLearning.translate)
+                        answers.forEach{ println(it.original)}
+                        val a = readln()
+                    }
+                } while (listOfUnlearnedWords.isNotEmpty())
+
+            }
+
             2 -> {
-                val percentsOfCorrectAnswers = ((quantityOfCorrectAnswers.size.toDouble() / dictionary.size) * 100).toInt()
+                val percentsOfCorrectAnswers =
+                    ((quantityOfCorrectAnswers.size.toDouble() / dictionary.size) * 100).toInt()
                 println("Выучено ${quantityOfCorrectAnswers.size} из ${dictionary.size} слов | ${percentsOfCorrectAnswers}%")
             }
 

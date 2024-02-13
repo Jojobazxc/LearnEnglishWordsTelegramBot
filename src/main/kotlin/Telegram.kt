615fe906a8f696fac1e6a7529ec68c05b7e1547a
Rodion <piperov2004@mail.ru>
2024-02-13 15:27:41 +0300

    added Telegram file

---
+++ src/main/kotlin/Telegram.kt
@@ -0,0 +1,33 @@
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
+
+fun main(args: Array<String>) {
+
+    val botToken = args[0]
+    var updateId = 0
+
+    while (true) {
+        Thread.sleep(2000)
+        val updates = getUpdate(botToken, updateId)
+        println(updates)
+
+        val startUpdateId = updates.lastIndexOf("update_id")
+        val endUpdateId = updates.lastIndexOf(",\n\"message\"")
+        if (startUpdateId == -1 || endUpdateId == -1) continue
+        val updateIdString = updates.substring(startUpdateId + 11, endUpdateId)
+
+        updateId = updateIdString.toInt() + 1
+    }
+    
+}
+
+fun getUpdate(botToken: String, updateId: Int): String {
+    val urlGetUpdates = "https://api.telegram.org/bot$botToken/getUpdates?offset=$updateId"
+    val client = HttpClient.newBuilder().build()
+    val request = HttpRequest.newBuilder().uri(URI.create(urlGetUpdates)).build()
+    val response = client.send(request, HttpResponse.BodyHandlers.ofString())
+
+    return response.body()
+}

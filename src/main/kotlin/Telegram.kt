a31054ae0a68d7fe14e48df642e6eff41095e06f
Rodion <piperov2004@mail.ru>
2024-02-25 19:23:13 +0300

    added sendMessage

--- src/main/kotlin/Telegram.kt
+++ src/main/kotlin/Telegram.kt
@@ -1,7 +1,9 @@
 import java.net.URI
+import java.net.URLEncoder
 import java.net.http.HttpClient
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse
+import java.nio.charset.StandardCharsets
 
 fun main(args: Array<String>) {
 
@@ -19,6 +21,24 @@ fun main(args: Array<String>) {
         val updateIdString = updates.substring(startUpdateId + 11, endUpdateId)
 
         updateId = updateIdString.toInt() + 1
+
+        val messageTextRegex: Regex = "\"text\":\"(.+?)\"".toRegex()
+        val matchResult: MatchResult? = messageTextRegex.find(updates)
+        val groups = matchResult?.groups
+        val text = groups?.get(1)?.value
+        println(text)
+
+
+
+        val chatIdRegex: Regex = "\"chat\":\\{\"id\":(\\d+)".toRegex()
+        val matchResultId: MatchResult? = chatIdRegex.find(updates)
+        val groupsId = matchResultId?.groups
+        val id = groupsId?.get(1)?.value
+        println(id)
+
+        val message = sendMessage(botToken, id, text)
+        println(message)
+
     }
     
 }
@@ -27,6 +47,16 @@ fun getUpdate(botToken: String, updateId: Int): String {
     val urlGetUpdates = "https://api.telegram.org/bot$botToken/getUpdates?offset=$updateId"
     val client = HttpClient.newBuilder().build()
     val request = HttpRequest.newBuilder().uri(URI.create(urlGetUpdates)).build()
+    val response = client.send(request, HttpResponse.BodyHandlers.ofString())
+
+    return response.body()
+}
+
+fun sendMessage(botToken: String, chatId: String?, textMessage: String?): String{
+    val encodedText = URLEncoder.encode(textMessage)
+    val urlSendMessage = "https://api.telegram.org/bot$botToken/sendMessage?chat_id=$chatId&text=$encodedText"
+    val client = HttpClient.newBuilder().build()
+    val request = HttpRequest.newBuilder().uri(URI.create(urlSendMessage)).build()
     val response = client.send(request, HttpResponse.BodyHandlers.ofString())
 
     return response.body()
